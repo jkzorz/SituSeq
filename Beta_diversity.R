c72802d9208cdc8d6720cc4d605463f0fee4d350
jkzorz <51386591+jkzorz@users.noreply.github.com>
2022-06-30 15:53:24 -0600

    Update Beta_diversity.R

--- Beta_diversity.R
+++ Beta_diversity.R
@@ -61,6 +61,68 @@ abund_temp = mantel(dist.abund, dist.temp, method = "spearman", permutations = 9
 
 
 #indicator species 
+library(indicspecies)
 inv = multipatt(m_com, data.scores2$Site, func = "r.g", control = how(nperm=9999))
 summary(inv)
+
+
+####Illumina alone 
+ setwd("~/University of Calgary/PostDoc/Atlantic Condor 2021/UofC_Analysis/Seaquencing/Illumina_reads/run_separation")
+tax_df = read.csv("seaquencing_ASVseq_taxa_4analysis.csv", header = TRUE)
+library(reshape2)
+library(tidyverse)
+library(vegan)
+
+tax_df2 = tax_df %>% filter(Kingdom == "Bacteria")
+tax_df3 = tax_df2 %>% select(-c(ASVID, Kingdom, Phylum, Class, Order, Family, Genus))
+
+sums = colSums(tax_df3)
+abund = t(t(tax_df3)/sums*100)
+abund2 = data.frame(Phylum = tax_df2$Phylum, abund)
+phy = abund2 %>% group_by(Phylum) %>% summarize_all(list(sum))
+phy$Phylum[is.na(phy$Phylum)] <- "Unknown"
+
+tax_dft = t(phy[,-1])
+colnames(tax_dft) = phy$Phylum
+m_com = as.matrix(tax_dft[,2:ncol(tax_dft)])
+
+set.seed(123)
+nmds = metaMDS(m_com, distance = "bray")
+nmds
+#stress: 0.08325794 
+
+
+data.scores = as.data.frame(scores(nmds)$sites)
+data.scores$Sample = colnames(phy[,2:ncol(phy)])
+
+data.scores2 = data.scores %>% separate(Sample, into = c("Extractor", "Year", "Ship", "Site", "Core", "Subsite", "Depth1", "Depth2", "Primer", "Date"), sep = "\\.")
+
+
+xx = ggplot(data.scores2, aes(x = NMDS1, y = NMDS2)) + geom_point(aes(colour = Subsite, size = as.numeric(Depth1))) + theme(panel.background = element_blank(), panel.border = element_rect(fill = NA, colour = "grey20"), legend.key = element_blank()) + scale_colour_manual(values = c("#ED254E", "#023778", "#A9E2A2", "#94A2B3", "#F9DC5C")) + scale_radius(range = c(1,6)) + labs(size = "Depth (cm)")
+
+xx = ggplot(data.scores2, aes(x = NMDS1, y = NMDS2)) + geom_point(aes(colour = Site, size = as.numeric(Depth1))) + theme(panel.background = element_blank(), panel.border = element_rect(fill = NA, colour = "grey20"), legend.key = element_blank()) + scale_colour_manual(values = c("#ED254E", "#023778", "#A9E2A2", "#94A2B3", "#F9DC5C")) + scale_radius(range = c(1,6)) + labs(size = "Depth (cm)")
+
+
+#anosim
+#site
+ano = anosim(m_com, data.scores2$Site, distance = "bray", permutations = 9999)
+#R: 0.4519, p< 1e-4
+
+#subsite
+ano = anosim(m_com, data.scores2$Subsite, distance = "bray", permutations = 9999)
+#R: 0.3749, p<1e-4
+
+#mantel
+dist.abund = vegdist(m_com, method = "bray")
+dist.temp = dist(data.scores2$Depth1, method = "euclidean")
+abund_temp = mantel(dist.abund, dist.temp, method = "spearman", permutations = 9999, na.rm = TRUE)
+#r: 0.3505, p < 1e-4
+
+#indicator species 
+library(indicspecies)
+inv = multipatt(m_com, data.scores2$Site, func = "r.g", control = how(nperm=9999))
+summary(inv)
+
+
+
 
