fdacb623a4a243eace976307b95b93bfeedb4b06
jkzorz <51386591+jkzorz@users.noreply.github.com>
2022-07-07 10:43:29 -0600

    Update mock_community.R

--- mock_community.R
+++ mock_community.R
@@ -2,7 +2,11 @@
 setwd("~/University of Calgary/PostDoc/Atlantic Condor 2021/UofC_Analysis/Seaquencing/Mock_community_Winand_2020")
 
 
+library(ShortRead)
+library(dada2)
+library(tidyverse)
 
 
-
+#filter and trim nanopore sequences
+out = filterAndTrim("Winand_2020_Long_read_SRR10391201.fastq.gz", "Winand_2020_Long_read_filt.fastq.gz", trimLeft = 100, trimRight = 100, maxLen = 1800, minLen = 1200,  truncQ = 0, compress = FALSE)
 
