d3e4562f0e32d4d49eb7649922967d63758dc778
jkzorz <51386591+jkzorz@users.noreply.github.com>
2022-07-07 10:48:14 -0600

    Update mock_community.R

--- mock_community.R
+++ mock_community.R
@@ -9,4 +9,19 @@ library(tidyverse)
 
 #filter and trim nanopore sequences
 out = filterAndTrim("Winand_2020_Long_read_SRR10391201.fastq.gz", "Winand_2020_Long_read_filt.fastq.gz", trimLeft = 100, trimRight = 100, maxLen = 1800, minLen = 1200,  truncQ = 0, compress = FALSE)
+head(out,12) 
+
+write.csv(out, "Filtered_sequence_summary.csv")
+
+#assign taxonomy
+#read in filtered fastq
+seqs = getSequences("Winand_2020_Long_read_filt.fastq.gz")
+#subset filtered fastq file to 5000 reads
+sub = sample(1:length(seqs), 5000, replace=FALSE)
+seq2 = seqs[sub]
+rm(seqs)
+
+#assign taxonomy
+tax_rc = assignTaxonomy(seq2, "../silva_nr99_v138.1_train_set.fa.gz", multithread=TRUE, tryRC = TRUE)
+write.csv(tax_rc, "Mock_community_nanopore_taxonomy.csv")
 
