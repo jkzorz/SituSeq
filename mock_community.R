2f6175dc1c33c8b155e4fe80d3cdd1773632d8ed
jkzorz <51386591+jkzorz@users.noreply.github.com>
2022-07-07 10:34:53 -0600

    Create mock_community.R

---
+++ mock_community.R
@@ -0,0 +1,8 @@
+#Use previously sequenced (Winand et al. 2020) mock community Nanopore and Illumina data to compare methods 
+setwd("~/University of Calgary/PostDoc/Atlantic Condor 2021/UofC_Analysis/Seaquencing/Mock_community_Winand_2020")
+
+
+
+
+
+
