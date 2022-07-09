09e3b17b37e90b847700ee1c20ebcfb781fd199a
jkzorz <51386591+jkzorz@users.noreply.github.com>
2022-07-09 12:33:16 -0600

    Update PhyloFlash_compare.R

--- PhyloFlash_compare.R
+++ PhyloFlash_compare.R
@@ -6,3 +6,15 @@ library(vegan)
 
 
 
+pf = read.csv("PhyloFlash_Relative_abundance.csv", header = TRUE)
+pf2 = pf %>% group_by(Phylum,Sample) %>% summarize(Abund = sum(Abund))
+pf3 = pf2 %>% pivot_wider(names_from = Sample, values_from = Abund)
+colnames(pf3)[2:ncol(pf3)] = paste("PF",  colnames(pf3)[2:ncol(pf3)], sep = "-")
+
+tax_comb = full_join(tax_df, nano, by = "Phylum")
+tax_comb0 = full_join(tax_comb, pf3, by = "Phylum")
+tax_comb2 = tax_comb0 %>% pivot_longer(!Phylum, names_to = "Sample", values_to = "Abundance") %>% separate(Sample, into = c("Tech", "Sample"), sep = "-")
+
+tax_comb3[,3:5][is.na(tax_comb3[,3:5])] <- 0
+
+
