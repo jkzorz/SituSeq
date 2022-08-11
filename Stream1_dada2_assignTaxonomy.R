181b51a254d63863d4237505bf54d94a0086a292
jkzorz <51386591+jkzorz@users.noreply.github.com>
2022-08-11 09:32:19 -0600

    Update Stream1_dada2_assignTaxonomy.R

--- Stream1_dada2_assignTaxonomy.R
+++ Stream1_dada2_assignTaxonomy.R
@@ -1,17 +1,21 @@
 ##This code can be copied and pasted directly into R to assign taxonomy to full length 16S rRNA reads sequenced using the Nanopore MinION platform and 16S barcode kit.
 ##The working directory folder should be set to the folder containing subfolders of fastq files from each barcoded sample. E.g From the Nanopore default output, the "fastq_pass" folder would be the working directory
-##Each subdirectory should start with "barcode" 
+##Each subdirectory should start with "barcode". If you would like to add extra identifying text, please add it to the subdirectory with underscores after "barcode". E.g. "barcode01_sample1" 
 ##You can change the parameters in the following section before running the code 
 
 
 ##########################################
 ##parameters to set before running
-subsample_depth = 1000 #for no subsampling see Nanopore_no_rarefaction.R
+subsample_depth = 1000 #each sample will be randomly subsampled to this number of reads. For no subsampling see Nanopore_no_rarefaction.R
 taxonomic_level = "Phylum" #choose from "Phylum" "Class" "Order" "Family" "Genus" 
 sample_number = 12
 path_to_taxonomy_database = "silva_nr99_v138.1_train_set.fa.gz" #change to location of taxonomy database in relation to working directory (easiest to copy database to working directory)
-path_to_working_directory = "."
-
+path_to_working_directory = "." #leave as a "." if you want to set your working directory manually in RStudio "Session"--> "Set Directory" --> "Choose Directory"
+#parameters for the filterAndTrim command:
+minLength = 1200 #Removes reads shorter than this length. Minimum length is enforced AFTER trimming
+maxLength = 1800 #Removes reads longer than this length. Maximum length is enforced BEFORE trimming 
+trimLeft = 100 #The number of nucleotides to remove from the start of each read. Must cover the primer length
+trimRight = 100 #The number of nucleotides to remove from the end of each read. Must cover the primer length
 
 ########################################
 
@@ -22,7 +26,7 @@ library(dada2)
 library(tidyverse)
 
 
-#try concatenating nanopore files in R
+#Concatenate nanopore files in R
 folders <- list.files(pattern = "barcode" )
 
 
@@ -46,26 +50,27 @@ path = getwd()
 #barcode01_combined.fastq 
 fnFs = sort(list.files(path, pattern="_combined.fastq", full.names = TRUE))
 
-#Extract sample names, assuming filenames have format: #samplename_XXX.fastq.gz
+#Extract sample names, assuming filenames have format: #samplename_XXX.fastq
 sample.names = sapply(strsplit(basename(fnFs), "\\."), `[`, 1)
-
-#need to get rid of "gz" because file is no longer zipped 
+ 
 
 #filter and trim reads- create new paths for new files 
 filtFs <- file.path(path, "filtered", paste0(sample.names, "_filt.fastq"))
 names(filtFs) = sample.names
 
 
-#filter and trim command - compress true or false?, originally 1350 and 1850, try 1200 and 1800?
-out = filterAndTrim(fnFs, filtFs, trimLeft = 100, trimRight = 100, maxLen = 1800, minLen = 1200,  truncQ = 0, compress = FALSE)
+#filter and trim command - 1200 and 1800?
+out = filterAndTrim(fnFs, filtFs, trimLeft = trimLeft, trimRight = trimRight, maxLen = maxLength, minLen = minLength,  truncQ = 0, compress = FALSE)
 #see how many reads were lost
 head(out,12) 
 write.csv(out, "Filtered_sequence_summary.csv")
 
+###########################################################################################################################################################
 ##########################Stop here if just using the R code for filtering and trimming sequences, output from this point can also be used for Stream 2
-########################################################################################################################################################
+###########################################################################################################################################################
 
 #for loop for getting sequences and assigning taxonomy - with subsetting to subsampling depth
+#this will create a csv file for each sample with the sequence and its assigned taxonomy
 for (fastq in filtFs) {
 print(fastq)
 seqs = getSequences(fastq)
@@ -93,7 +98,7 @@ for (i in 1:length(temp)) {
 #merge all data frames in list
 tax_df = temp_list %>% reduce(full_join, by=taxonomic_level)
 
-#write phylum summary csv
+#write summary csv of taxonomic level 
 write.csv(tax_df, paste0(taxonomic_level,"_summary.csv"))
 
 #long format
@@ -118,7 +123,7 @@ xx = ggplot(tax_df_long, aes(x = Sample, y = reorder(get(taxonomic_level), desc(
  
 colours = c("#2F4858", "#33658A", "#86BBD8", "#830689", "#F5A614", "#F26419", "#BB3551",  "#C1D7AE", "#68AC5D", "#EBDDAD")
  
- tax_df2_long = tax_df2 %>% select(-max) %>% pivot_longer(!taxonomic_level, names_to = "Sample", values_to = "Abundance")
+tax_df2_long = tax_df2 %>% select(-max) %>% pivot_longer(!taxonomic_level, names_to = "Sample", values_to = "Abundance")
 
 tax_df2_long$Sample = gsub("_combined","",tax_df2_long$Sample)
 
