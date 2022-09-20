c14624cfd7e8cc2b1b1fc2f4325811acdec8c3fc
jkzorz <51386591+jkzorz@users.noreply.github.com>
2022-09-20 16:33:36 -0600

    Update Stream2_blast_database.sh

--- Stream2_blast_database.sh
+++ Stream2_blast_database.sh
@@ -9,17 +9,17 @@
 #open your command line program (e.g. wsl in windows, Terminal on a mac or a linux system) 
 
 #move into directory containing filtered and trimmed sequences from the Preprocessing.R script e.g. cd filtered
-#convert all fastq files to fasta files
+#convert all fastq files to fasta files - will create new fasta files 
 for i in *.fastq; do sed -n '1~4s/^@/>/p;2~4p' $i > $(basename $i fastq)fasta; done
 
-#append sample name to fasta header. Fasta files with sample name in header are saved as "header_samplename"
+#append sample name to fasta header. Fasta files with sample name in header are saved in a new file: "header_samplename"
 for i in *.fasta; do name="$(basename $i _combined_filt.fasta)"; sed "s/>/>${name} /g" $i > 'header_'$(basename $i); done
 
 #concatenate all Nanopore fasta files with changed headers together
 cat header_* > NanoporeSeqs.fasta
 
 #Now you need a fasta file containing the sequences of interest: e.g. seqs.fasta
-#move into the directory containing your seqs.fasta file with the cd command 
+#move into the directory containing your seqs.fasta file with the cd command (e.g. to go up a directory: cd ..)
 #make a BLAST database out of the fasta sequences found in seqs.fasta (change names accordingly) 
 
 makeblastdb -in seqs.fasta -out seqs_DB -dbtype nucl
@@ -28,7 +28,7 @@ makeblastdb -in seqs.fasta -out seqs_DB -dbtype nucl
 #note that 1 max_target_seqs is not recommended when the user is interested in the "best" hit 
 #as the blast algorithm only reports the first hit that matches the requirements, which may or may not be the "best" hit.
 #For this analysis, we are interested in counting the number of hits above 97% to our sequences of interest per sample, 
-#and so it does not specifically matter if the hits that are returned are the "best" hits.
+#and so it does not specifically matter if the hit that is returned for a given search is the "best" hit.
 #If using this code for a different purpose keep this in mind and change the -max_target_seqs parameter accordingly
 
 blastn -query filtered/NanoporeSeqs.fasta -db seqs_DB -outfmt 6 -out blast_results.tbl -max_target_seqs 1 -perc_identity 97
@@ -36,11 +36,11 @@ blastn -query filtered/NanoporeSeqs.fasta -db seqs_DB -outfmt 6 -out blast_resul
 ######
 #The rest of the analysis will be done in R
 ######
-#This first section is the analysis without normalizing for number of sequences per sample 
-#The following section normalizes for number of sequences per sample. Skip to the following section if you would rather calculate relative abundance of hits
-#than absolute number of hits
+#The following section performs the analysis without normalizing for number of sequences per sample 
+#The section afterwards normalizes for number of sequences per sample. 
+#Skip ahead if you would rather calculate relative abundance of hits than the absolute number of hits
 
-#open R and set your working directory
+#open R and set your working directory to the folder containing your "blast_results.tbl" file
 #e.g. setwd("~/My_working_directory")
 
 #load tidyverse library
@@ -55,10 +55,10 @@ blast_hq = blast %>% filter(V4 > 400)
 #or filter out hits less than a certain e-value 
 blast_hq = blast %>% filter(V11 <= 0)
 
-#Count the number of high quality hits per sample 
+#count the number of high quality hits per sample 
 blast_sum = blast_hq %>% group_by(V1) %>% summarize(n = n())
 
-#Plot the number of high quality hits per sample as bar plot
+#plot the number of high quality hits per sample as bar plot
 #colour scheme, change according to number of samples and desired colours
 colours = c("#2F4858","#68AC5D", "#830689", "#C1D7AE",   "#86BBD8",  "#F5A614","#33658A",  "#BB3551",  "#F26419",  "#EBDDAD")
 
@@ -70,7 +70,7 @@ gg
 ggsave("Blast_hits_per_sample.png", height = 6, width = 6)
 
 #If also interested in which of the sequences of interest had hits use the following code to plot. 
-#If there are many sequences of interest in the database (e.g. >40) the figure will get crowded
+#If there are many sequences of interest in the database (e.g. >30) the figure will get crowded
 blast_sum_seqs = blast_hq %>% group_by(V1,V2) %>% summarize(n = n())
 
 #plot as a bar plot
@@ -82,7 +82,7 @@ ggsave("Blast_hits_per_sample_and sequence.png", height = 8, width = 7)
 
 #######################################################################
 #This code is for calculating relative abundance of hits
-#You will need the Filtered_sequence_summary.csv file from the Stream 1 Filter and Trim step
+#You will need the Filtered_sequence_summary.csv file from the Preprocessing.R script filterAndTrim step
 
 #open R and set your working directory
 #e.g. setwd("~/My_working_directory")
@@ -112,7 +112,7 @@ filt$X = gsub("_combined.fastq.*", "", filt$X)
 filt2 = data.frame(V1 = filt$X, num_seqs = filt$reads.out)
 
 #join the blast_sum and filt2 data frames
-blast_sum2 = left_join(blast_sum, filt2, "V1")
+blast_sum2 = full_join(blast_sum, filt2, "V1")
 
 #calculate relative abundance of sequences of interest
 blast_sum2$rel_abund = (blast_sum2$n/blast_sum2$num_seqs)*100
@@ -129,11 +129,11 @@ ggsave("Blast_hits_per_sample_percent.png", height = 6, width = 6)
 
 
 #If also interested in which of the sequences of interest had hits use the following code to plot. 
-#If there are many sequences of interest in the database (e.g. >40) the figure will get crowded
+#If there are many sequences of interest in the database (e.g. >30) the figure will get crowded
 blast_sum_seqs = blast_hq %>% group_by(V1,V2) %>% summarize(n = n())
 
 #join the blast_sum_seqs and filt2 data frames
-blast_sum_seqs2 = left_join(blast_sum_seqs, filt2, "V1")
+blast_sum_seqs2 = full_join(blast_sum_seqs, filt2, "V1")
 
 #calculate relative abundance of sequences of interest
 blast_sum_seqs2$rel_abund = (blast_sum_seqs2$n/blast_sum_seqs2$num_seqs)*100
