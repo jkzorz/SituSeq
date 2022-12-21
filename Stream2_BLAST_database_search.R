7fab325354446be0ca3f2547347f7f5fdac8a55e
jkzorz <51386591+jkzorz@users.noreply.github.com>
2022-12-21 14:20:50 -0700

    Update Stream2_BLAST_database_search.R

--- Stream2_BLAST_database_search.R
+++ Stream2_BLAST_database_search.R
@@ -9,7 +9,10 @@
 ######
 #parameters to set before running:
 path_to_working_directory = "." #leave as a "." if you want to set your working directory manually in RStudio "Session"--> "Set Directory" --> "Choose Directory"
-path_to_seqs4_BLAST_db = "BLAST_DB_seqs.fasta" #change to file name and location of sequences to use for BLAST database
+path_to_seqs4_BLAST_db = "BLAST_DB.fasta" #change to file name and location of sequences to use for BLAST database
+#to add: 
+#perc identity cutoff for blast
+#number of samples
 ######
 
 #load packages
@@ -18,10 +21,10 @@ library(ShortRead)
 library(tidyverse)
 
 #set working directory
-setwd(path_to_working_directory)
+setwd(paste0(path_to_working_directory,"/filtered"))
 
 #create fasta files from filtered fastq files, and change headers to contain sample names
-k = list.files(setwd("filtered"), pattern = "_filt.fastq", full.names = TRUE)
+k = list.files(pattern = "_filt.fastq", full.names = TRUE)
 for (fq in k) {
   fq2 = basename(fq)
   fa = gsub("fastq", "fasta",fq2)
@@ -38,7 +41,7 @@ for (fq in k) {
 m <- list.files(pattern = "_filt.fasta" )
 for (fa in m) {
   print(fa) 
-  fout = file.path("All_combined_seqs.fasta")
+  fout = file.path("../All_combined_seqs.fasta")
   writeFasta(readFasta(fa), fout, mode = "a")
 }
 
@@ -46,17 +49,19 @@ for (fa in m) {
 setwd("../")
 seq <- readDNAStringSet("All_combined_seqs.fasta")
 
-#change BLAST_DB to your fasta file
-makeblastdb("BLAST_DB.fasta", dbtype = "nucl")
-dbb <- blast(db="BLAST_DB.fasta")
+#Make BLAST db and perform BLAST search 
+makeblastdb(path_to_seqs4_BLAST_db, dbtype = "nucl")
+dbb <- blast(db=path_to_seqs4_BLAST_db)
+#change parameters here as required
 results = predict(dbb, seq, BLAST_args= c("-max_target_seqs 1", "-perc_identity 97"))
 write.csv(results, "blast_results.csv", row.names = FALSE)
 
 
-#############################same as before
+#############################
+##Processing BLAST results##
+
 #load blast results table 
 blast = read.csv("blast_results.csv",  header = FALSE)
-
 
 #filter out hits that are smaller than expected size
 blast_hq = blast %>% filter(V4 > 400)
