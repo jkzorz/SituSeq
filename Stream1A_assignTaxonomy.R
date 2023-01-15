fcaf2a496215cdcf262874a90a5871144f60c444
jkzorz <51386591+jkzorz@users.noreply.github.com>
2023-01-15 09:26:11 -0700

    Update Stream1A_assignTaxonomy.R
    Split Stream1 into Stream1A and Stream 1B. Stream 1A performs the time-consuming step of assigning taxonomy. Stream 1B allows the user to easily explore and visualize the data at different taxonomic levels.

--- Stream1A_assignTaxonomy.R
+++ Stream1A_assignTaxonomy.R
@@ -8,8 +8,6 @@
 ######
 #parameters to set before running
 subsample_depth = 1000 #each sample will be randomly subsampled to this number of reads, prior to taxonomic assignment (after filtering and trimming). For no subsampling see Nanopore_no_rarefaction.R under "backups" 
-taxonomic_level = "Phylum" #choose from "Phylum" "Class" "Order" "Family" "Genus" 
-sample_number = 12
 path_to_taxonomy_database = "silva_nr99_v138.1_train_set.fa.gz" #change to location of taxonomy database in relation to working directory (easiest to copy taxonomy database to working directory)
 path_to_working_directory = "." #leave as a "." if you want to set your working directory manually in RStudio "Session"--> "Set Directory" --> "Choose Directory"
 ######
